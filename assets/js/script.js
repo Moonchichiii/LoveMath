6223925c19dffb974c2c6e08b7fc169adf110d55
Mats Gustafsson <mats.gustafsson83@gmail.com>
2023-02-07 19:29:47 +0000

    css js.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,36 @@
-    document.addEventListener("DOMContentLoaded" , function() {
+document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
     
-    for (let button of buttons){
-        button.addEventListener('click' , function() {
-            if (this.getAttribute("data-type") === "submit")
-            alert("You clicked Submit");
+    for (let button of buttons) {
+      button.addEventListener('click', function() {
+        if (this.getAttribute("data-type") === "submit") {
+          alert("You clicked Submit");
         } else {
-            let gameType = this.getAttribute("data-type"),
-            alert('You clicked , ${gameType}');
-    
-    
+          let gameType = this.getAttribute("data-type");
+          alert(`You clicked ${gameType}`);
         }
-    
-        runGame("addition")
-    }}
-
-
-
-/**
- * the main game "loop", called when the script is first loaded
- * and after the usere's answer har been processed
- */
-function runGame(gameType){
-    let num1 = Math.floor(Math.random() * 25) +1;
-    let num2 = Math.floor(Math.random() * 25) +1;
-
-    if(gameType === "addition"){
-        displayAdditionQuestion(num1,num2);
-        else{
-            alert('Unknown game type: ${gameType}');
-            throw 'Unknown game type' ${gameType}. aborting!;
-        }
+      });
     }
-
-
-
-}
-
+    
+    runGame("addition");
+  });
+  
+  /**
+   * the main game "loop", called when the script is first loaded
+   * and after the user's answer has been processed
+   */
+  function runGame(gameType) {
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
+  
+    if (gameType === "addition") {
+      displayAdditionQuestion(num1, num2);
+    } else {
+      alert(`Unknown game type: ${gameType}`);
+      throw `Unknown game type: ${gameType}. Aborting!`;
+    }
+  }
+  
 
 function checkAnswer() {
 
@@ -57,7 +51,7 @@ function incrementWrongAnswer() {
 function displayAdditionQuestion(operand1, operand2) {
 document.getElementById('operand1').textContent = operand1;
 document.getElementById('operand2').textContent = operand2;
-document.getElementById('operator').textContent = operator;
+document.getElementById('operator').textContent = "+";
 
 }
 
